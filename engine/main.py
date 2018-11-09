270341bc066587243dca32f26cae0b57772e2aa2
KaiserMovet <rynkiewicz.mate@gmail.com>
2018-11-09 01:36:29 +0100

    added start.sh, tdev.py create yaml

---
+++ engine/main.py
@@ -0,0 +1,10 @@
+import config as con
+
+def main():
+    print("Hello World!")
+    print(con.path)
+    
+  
+if __name__== "__main__":
+    main()
+
---
+++ engine/types.py
@@ -0,0 +1,18 @@
+import sys
+from ruamel.yaml import YAML
+import ../k-smart.conf
+
+class Types(object):
+    def __init__(self):
+        self.name = "None"
+        self.maxValue = 255
+        isRec = "true"
+        self.desc = "Some Device Type"
+    def to_file(self):
+        yaml = YAML()
+        yaml.register_class(Types)
+        yaml.dump(self, sys.stdout)
+
+
+temp = Types()
+temp.to_file()
