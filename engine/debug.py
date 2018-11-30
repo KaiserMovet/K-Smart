31316ec1deea1a6cdeea31d2330afc4e725c3ae0
KaiserMovet <rynkiewicz.mate@gmail.com>
2018-11-30 01:31:20 +0100

    gitignore changed

--- engine/debug.py
+++ engine/debug.py
@@ -2,6 +2,6 @@ import datetime
 datetime.datetime.now()
 def Log(string):
     message=str(datetime.datetime.now().strftime("[%Y-%m-%d %H:%M:%S:%f]\t"+string+";\n"))
-    print(message)
-    with open("log.txt","a+") as file:
-            file.write(message)
+    #print(message)
+    #with open("log.txt","a+") as file:
+    #        file.write(message)
